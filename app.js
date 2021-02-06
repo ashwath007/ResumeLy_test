12513c48d6d956582c08e5c7520cf090df9e5629
ashwath007 <srivicky2000@gmail.com>
2021-02-06 08:28:52 +0000

    app.js 1

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var cors = require('cors');
+var cookieParser = require('cookie-parser');
 
-
+app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine',ejs);
+app.set('view engine','ejs');
+app.use(cookieParser());
+
+
+
+//DATABASE
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+    console.log(chalk.green.bold("DONE"));
+});
+
+//
+
 
 
 app.get("/",function(req,res){
