7f6bd3f2c96717eec021b01a3a2dd0e7aec016bb
ashwath007 <srivicky2000@gmail.com>
2021-02-06 08:22:41 +0000

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const chalk = require('chalk');
 const port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var cors = require('cors');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine',ejs);
+
 
 app.get("/",function(req,res){
 		res.send("ResumeLY");
